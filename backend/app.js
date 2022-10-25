ae24b8739eb2013f578f946bf0fe88879529f74a
AbdouScript <abdoukichenama@gmail.com>
2022-10-25 10:58:37 +0200

    création application express

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,5 @@
-const express = require ('express');
+const express = require("express");
+
 const app = express();
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' })
-});
-
-// API MIDDLEWARES
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.static('public'));
-
-
-
-// API ROUTES
-app.get('./register', (req, res) => {
-
-    res.sendFile(__dirname + '../register.html');
-
-})
-
-app.post('/formPost', (req, res) => {
-    console.log(req.body);
-})
 
 module.exports = app;
